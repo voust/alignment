3daa62eb8c63553fc2dccfe16d88adac3b8fce19
voust <voust@users.noreply.github.com>
2026-02-18 04:31:42 +0000

    deploy: 3710830d15a0c7ac83c41b288b070c7e9ffca3df

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-db2de0ac.js',
+                path_to_root + 'searchindex-2f465dce.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
